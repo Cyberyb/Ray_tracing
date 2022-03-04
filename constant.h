0d8626d55f483de49c92f487d980283b6bf5fd49
Cyberyb <cyberybhshu@163.com>
2022-03-05 00:14:15 +0800

    Antialiasing

--- constant.h
+++ constant.h
@@ -3,6 +3,7 @@
 #include<cmath>
 #include<limits>
 #include<memory>
+#include<cstdlib>
 
 //Usings
 using std::make_shared;
@@ -18,6 +19,27 @@ const double pi = 3.1415926535897932385;
 double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+double random_double()
+{
+    //Return [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double random_double(double min,double max)
+{
+    //Return a random real in [min,max)
+    return min + (max - min) * random_double();
+}
+
+double clamp(double x,double min,double max)
+{
+    if(x<min)
+        return min;
+    if(x>max)
+        return max;
+    return x;
 }
 
 //Common Headers
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 
@@ -26,6 +27,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     //World
     hittable_list world;
@@ -33,37 +35,26 @@ int main() {
     world.add(make_shared<sphere>(point3(0,-100.5,-1),100));//添加背景球体
     
     //Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0; // set the distance between the projection plane and the projection point to be one unit
-
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width,0,0);//viewport x
-    auto vertical = vec3(0,viewport_height,0);//viewport y
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 -vec3(0,0,focal_length);//
+    camera cam;
 
     // Render
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height-1; j >= 0; --j) 
+    {
         std::cerr << "\rScanlines remaining: " << j << ' '<<std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            // auto r = double(i) / (image_width-1);
-            // auto g = double(j) / (image_height-1);
-            // auto b = 0.25;
-
-            // int ir = static_cast<int>(255.999 * r);
-            // int ig = static_cast<int>(255.999 * g);
-            // int ib = static_cast<int>(255.999 * b);
-
-            // std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r,world);
-            write_color(std::cout,pixel_color);
-            
+        for (int i = 0; i < image_width; ++i) 
+        {
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel;++s)
+            {
+                auto u = double(i + random_double()) / (image_width - 1);
+                auto v = double(j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u,v);
+                pixel_color += ray_color(r,world);
+            }
+            write_color(std::cout,pixel_color,samples_per_pixel);            
         }
     }
     std::cerr<<"\nDone\n";
